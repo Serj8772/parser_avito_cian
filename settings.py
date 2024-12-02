197da8b9b87222861b50e409bfccb6fe03584d9e
Serj8772 <icq66623702@ya.ru>
2024-12-02 18:40:10 +0300

    1

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-token = ''    # телеграм бот
-id_channel = ''   # телеграм канал
+token = '8117321088:AAGqq9vLFLbkGpbtKKgxjoDUSURvl_6glHU'    # телеграм бот
+id_channel = '-1002409932666'   # телеграм канал
 
 link_avito='https://www.avito.ru/lobnya/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?context=H4sIAAAAAAAA_wEjANz_YToxOntzOjg6ImZyb21QYWdlIjtzOjc6ImNhdGFsb2ciO312FITcIwAAAA&f=ASgBAgICA0SSA8gQ8AeQUrCzFP6hjwM&s=104'
 link_cian='https://lobnya.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&region=4848&room1=1&room2=1&sort=creation_date_desc&type=4'
